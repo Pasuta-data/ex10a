4bd210f31a40221319a84561d7c34cdca90a00a4
Pasuta-data <pasuta.p2001@gmail.com>
2025-04-07 18:46:32 +0700

    validate.js

--- validate.js
+++ validate.js
@@ -1,34 +1,26 @@
 function checkHeight() {
-  let h = (document.getElementById("myHeight").value).trim();
-  if (!isNaN(h)) {
-    return true;
-  } else {
-	return false;
-  }
+	let h = parseInt(document.getElementById("myHeight").value.trim());
+	return !isNaN(h) && h >= 1 && h <= 200;
 }
 
 function checkInseam() {
-  let i = (document.getElementById("myInseam").value).trim();
-  if (!isNaN(i)) {
-    return true;
-  } else {
-	return false;
-  }
+	let i = parseInt(document.getElementById("myInseam").value.trim());
+	return !isNaN(i) && i >= 1 && i <= 100;
 }
 
-function validateForm(){
-	if(!checkHeight()){
-	  alert("Invalid value for height!");
-	  document.getElementById("myHeight").focus();
-	  return false;
-	}else{
-		if(!checkInseam()){
-		  alert("Invalid value for inseam!!");
-		  document.getElementById("myInseam").focus();
-		  return false;
-		}else{
+function validateForm() {
+	if (!checkHeight()) {
+		alert("Invalid value for height!");
+		document.getElementById("myHeight").focus();
+		return false;
+	} else {
+		if (!checkInseam()) {
+			alert("Invalid value for inseam!!");
+			document.getElementById("myInseam").focus();
+			return false;
+		} else {
 			size = sizeCalculate();
-			alert("Bike size based on you height / insteam  is "+size);
+			alert("Bike size based on you height / insteam  is " + size);
 			return false;
 		}
 	}
